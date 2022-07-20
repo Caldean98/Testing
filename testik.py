6ce3311cd56e110cbbff8c10c6682e14defad624
Caldean98 <108994301+Caldean98@users.noreply.github.com>
2022-07-20 08:48:48 +0200

    Add files via upload

--- testik.py
+++ testik.py
@@ -1,10 +1,21 @@
 import requests
 import json
 
-base_url = "https://api.hypixel.net/skyblock/auctions_ended"
+base_url = 'https://api.hypixel.net/skyblock/auctions_ended'
 
 r = requests.get(base_url)
 
 data = r.json()
 
-print(data["auction_id"])
+json_obj = json.load(r)
+
+array = []
+for i in json_obj['auction_id']:
+    array[i] = i['auction_id']
+print(array)
+
+
+#auctions_all = (data["auctions"])
+
+#print(auctions_all)
+
